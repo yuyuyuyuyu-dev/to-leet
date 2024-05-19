de75791f3e4abbd189c93ca88dc7caac57585d1e
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2024-05-19 09:53:41 +0900

    ライセンスページを改良しました

--- src/app/licenses/licenses.component.ts
+++ src/app/licenses/licenses.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 import _licenses from "../../assets/3rd-party-licenses.json";
 import { RouterOutlet } from "@angular/router";
+import { MatExpansionModule } from "@angular/material/expansion";
 
 @Component({
   selector: "app-licenses",
   standalone: true,
   imports: [
     RouterOutlet,
+    MatExpansionModule,
   ],
   templateUrl: "./licenses.component.html",
   styleUrl: "./licenses.component.css",
